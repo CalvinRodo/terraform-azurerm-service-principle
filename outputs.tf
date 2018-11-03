e52d1b039a444dd7ac4919c2fec10e4632c44c6e
Calvin Rodo <calvin.rodo@hrsdc-rhdcc.gc.ca>
2018-11-02 22:51:23 -0400

    terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
 output "aks_role_name" {
   description = "Display name"
-  value = "aks_sp_role"
+  value       = "aks_sp_role"
 }
 
 output "sp_id" {
   description = "The id of the service principle"
-  value = "${azurerm_azuread_service_principal.aks_sp.id}"
+  value       = "${azurerm_azuread_service_principal.aks_sp.id}"
 }
 
 output "client_id" {
   description = "Client ID of the service principle"
-  value = "${azurerm_azuread_service_principal.aks_sp.application_id}"
+  value       = "${azurerm_azuread_service_principal.aks_sp.application_id}"
 }
 
 output "client_secret" {
-  sensitive = true
+  sensitive   = true
   description = "Password of the service principle"
-  value     = "${random_string.aks_sp_password.result}"
+  value       = "${random_string.aks_sp_password.result}"
 }
